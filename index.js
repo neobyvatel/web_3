2a75c61b2f2984e6f3907d8b54b7f008776ecc66
Adil <skeezygod@gmail.com>
2024-02-10 23:36:00 +0600

    pages

--- index.js
+++ index.js
@@ -12,6 +12,38 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("home");
 });
+app.get("/weather", (req, res) => {
+  res.render("weather");
+});
+app.get("/github", (req, res) => {
+  res.render("githubUsers");
+});
+app.get("/stocks", (req, res) => {
+  res.render("stocks");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+// app.get("/user/:id", async (req, res) => {
+//   try {
+//     // Assuming UserModel has a method to find a user by their ID
+//     const user = await UserModel.findById(req.params.id);
+
+//     if (!user) {
+//       return res.status(404).send("User not found");
+//     }
+
+//     // Render the user info using the user.ejs template
+//     res.render("user", { user });
+//   } catch (error) {
+//     console.error("Error retrieving user:", error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
